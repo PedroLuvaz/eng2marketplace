6e6d45d19b880aa49ed0d9f82320f646bcfff49d
Otavio Ferreira <38397566+otavio-f@users.noreply.github.com>
2025-03-23 22:31:50 -0300

    Obtenção de dados Adicionado obtenção de dados

--- src/main/java/com/eng2marketplace/view/ConsoleInput.java
+++ src/main/java/com/eng2marketplace/view/ConsoleInput.java
@@ -1,5 +1,6 @@
 package com.eng2marketplace.view;
 
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Pattern;
@@ -11,6 +12,8 @@ import java.util.regex.Pattern;
 public class ConsoleInput {
     private final Scanner scanner = new Scanner(System.in);
 
+    public ConsoleInput() {
+    }
     /**
      * Obtém um texto qualquer com pelo menos um caractere.
      * O texto deve conter no mínimo um caractere.
@@ -32,6 +35,18 @@ public class ConsoleInput {
         return null;
     }
 
+    public String askText(String prompt, String regex, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getText(regex);
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
+    }
+
     /**
      * Obtém um e-mail digitado pelo usuário no padrão aaa@bbb.ccc
      * Os caracteres aceitos antes da arroba são alfanuméricos, ponto e underscore e hífen.
@@ -44,6 +59,18 @@ public class ConsoleInput {
         if(Pattern.matches("([A-z0-9_\\.-]+\\@[A-z0-9_-]+\\.[A-z\\.]{2,6})", input))
             return input;
         return null;
+    }
+
+    public String askMail(String prompt, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getMail();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
     }
 
     /**
@@ -65,16 +92,41 @@ public class ConsoleInput {
         return input;
     }
 
+    public String askName(String prompt, int maxSize, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getName(maxSize);
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
+    }
     /**
      * Obtém um número inteiro.
      * @return Um número ou null se um número inválido for informado.
      */
     public Integer getNumber() {
         try {
-            return this.scanner.nextInt();
+            int result = this.scanner.nextInt();
+            this.scanner.nextLine(); // scanner bug workaround
+            return result;
         } catch (InputMismatchException e) { // número veio troncho
             return null;
         }
+    }
+
+    public int askNumber(String prompt, String invalidMsg) {
+        Integer answer;
+        do {
+            System.out.print(prompt);
+            answer = getNumber();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
     }
 
     /**
@@ -92,6 +144,18 @@ public class ConsoleInput {
         return input;
     }
 
+    public int askNumber(String prompt, int min, int max, String invalidMsg) {
+        Integer answer;
+        do {
+            System.out.print(prompt);
+            answer = getNumber(min, max);
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
+    }
+
     /**
      * Obtém um número fracionário.
      * @return Um número ou null se nenhum número válido for informado
@@ -102,6 +166,18 @@ public class ConsoleInput {
         } catch (InputMismatchException e) { // número veio troncho
             return null;
         }
+    }
+
+    public double askValue(String prompt, String invalidMsg) {
+        Double answer;
+        do {
+            System.out.print(prompt);
+            answer = getValue();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
     }
 
     /**
@@ -117,6 +193,18 @@ public class ConsoleInput {
         if(input < min || input > max)
             return null;
         return input;
+    }
+
+    public double askValue(String prompt, double min, double max, String invalidMsg) {
+        Double answer;
+        do {
+            System.out.print(prompt);
+            answer = getValue(min, max);
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
     }
 
     /**
@@ -140,6 +228,18 @@ public class ConsoleInput {
         return null;
     }
 
+    public String askCPF(String prompt, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getCPF();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
+    }
+
     /**
      * Obtém um número de CNPJ.
      * Os padrões aceitos são XX.XXX.XXX/000Y-XX, composto de 10 números, pontos, barra e hífen,
@@ -161,5 +261,66 @@ public class ConsoleInput {
                 input.substring(12, 14));
         }
         return null;
+    }
+
+    public String askCNPJ(String prompt, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getCNPJ();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
+    }
+
+
+    /**
+     * Obtém um número de identificação (CPF ou CNPJ).
+     * Os padrões de CNPJ aceitos são XX.XXX.XXX/000Y-XX, composto de 10 números, pontos, barra e hífen,
+     * no qual o número Y é 1 ou 2.
+     * Outro padrão de CNPJ aceito é XXXXXXXX000YXX, composto de números, no qual o caractere Y é 1 ou 2.
+     * Os padrões de CPF válidos são XXX.XXX.XXX-XX, compostos de números, pontos e hífen,
+     * ou XXXXXXXXXXX, composto somente de números
+     * @return Um número de CPF ou CNPJ ou null caso não for informado um valor válido
+     */
+    public String getCPFCNPJ() {
+        String input = this.scanner.nextLine();
+        if(Pattern.matches("\\d{2}.\\d{3}.\\d{3}\\/(?:0001|0002)-\\d{2}", input))
+            return input;
+        if(Pattern.matches("\\d{8}(?:0001|0002)\\d{2}", input)){
+            return String.format(
+                "%s.%s.%s/%s-%s",
+                input.substring(0, 2),
+                input.substring(2, 5),
+                input.substring(5, 8),
+                input.substring(8, 12),
+                input.substring(12, 14));
+        }
+        if(Pattern.matches("\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", input))
+            return input;
+        if(Pattern.matches("\\d{11}", input)) {
+            return String.format(
+                "%s.%s.%s-%s",
+                input.substring(0, 3),
+                input.substring(3, 6),
+                input.substring(6, 9),
+                input.substring(9, 11));
+        }
+        return null;
+    }
+
+
+    public String askCPFCNPJ(String prompt, String invalidMsg) {
+        String answer;
+        do {
+            System.out.print(prompt);
+            answer = getCPFCNPJ();
+            if(answer == null)
+                System.out.println(invalidMsg);
+        } while (answer == null);
+
+        return answer;
     }
 }
